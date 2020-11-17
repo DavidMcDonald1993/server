51c495e7ca962c9acfd4ae0aad620f5c977e1a97
david <davemcdonald@live.co.uk>
2020-11-17 11:28:04 +0000

    added PASS functionality to mongoDB

--- pass_app/backend.py
+++ pass_app/backend.py
@@ -3,6 +3,75 @@ import os
 
 from django.utils.encoding import smart_str
 
+
+from datetime import datetime
+
+from pymongo import MongoClient
+
+import numpy as np
+import pandas as pd
+
+from rdkit.Chem.PandasTools import LoadSDF
+
+HOST = "192.168.0.49"
+PORT = 27017
+DB = "PASS_TEST"
+
+def connect_to_db():
+    print ("connecting to MongoDB database", 
+        DB, "using host",
+        HOST, "and port", PORT)
+    client = MongoClient(HOST, PORT)
+    return client[DB]
+
+def count_documents(collection, filter={}):
+    return collection.count_documents(filter=filter)
+
+def clear_collection(collection, filter={}):
+    collection.remove(filter=filter)
+
+def parse_pass_spectra(s, ):
+    split = s.split()
+    return {"Pa": split[0], "Pi": split[1], "activity" : " ".join(split[2:])}
+
+def write_PASS_hits_to_db(
+    pass_file,
+    collection="PASS_hits"):
+    assert isinstance(pass_file, str)
+    assert pass_file.endswith(".sdf")
+
+    pass_activities = LoadSDF(pass_file, 
+            smilesName='SMILES', molColName=None)
+
+    db = connect_to_db()
+
+    print ("writing PASS hits to MongoDB:", DB, 
+        "collection:", collection)
+
+    hit_collection = db[collection]
+
+    records = []
+
+    print ("determining hits")
+
+    for _, row in pass_activities.iterrows():
+        
+        entry = {}
+        for col in row.index:
+            if pd.isnull(row[col]):
+                continue
+            if col == "PASS_ACTIVITY_SPECTRUM":
+                value = list(map(parse_pass_spectra, row[col].split("\n")))
+            else:
+                value = row[col]
+            entry.update({col: value})
+        records.append(entry)
+
+    print ("inserting", len(records), "records")
+    hit_collection.insert_many(records)
+
+    db.client.close()
+
 def handle_uploaded_file(f):
 
     filename = f.name
@@ -22,6 +91,8 @@ def handle_uploaded_file(f):
     ret = os.system(cmd)
 
     assert ret == 0
+
+    # write PASS spectra to database 
 
     return smart_str(pass_out_file)
 
