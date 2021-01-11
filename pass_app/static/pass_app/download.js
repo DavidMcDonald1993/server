90b82585fad40acc5b0a6344059ccd0be6ac3e15
david <davemcdonald@live.co.uk>
2021-01-11 16:02:38 +0000

    download error page

--- pass_app/static/pass_app/download.js
+++ pass_app/static/pass_app/download.js
@@ -1,8 +1,8 @@
 $(function() {
-    $('a data-auto-download').each(function(){
-        var $this = $(this);
-        setTimeout(function() {
-            window.location = $this.attr('href');
-         }, 1000);
+    $('a[data-auto-download]').each(function(){
+      var $this = $(this);
+      setTimeout(function() {
+        window.location = $this.attr('href');
+      }, 2000);
     });
-});
+  });
